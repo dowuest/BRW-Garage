9494df5b834d3021bedad8c37015e2fe83164e06
dowuest <dowuest@gmail.com>
2026-04-30 08:40:46 +0200

    Fix [object Object]: renderField helper for nested arrays/objects/booleans

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'brw-v8';
+const CACHE = 'brw-v9';
 const ASSETS = ['/BRW-Garage/', '/BRW-Garage/index.html', '/BRW-Garage/manifest.json', '/BRW-Garage/icon-192.png', '/BRW-Garage/icon-512.png'];
 
 self.addEventListener('install', e => {
