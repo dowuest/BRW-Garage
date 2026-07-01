7128eb1e1749d68743baafebdcac1d4f92f0cef2
dowuest <dowuest@gmail.com>
2026-07-01 23:37:32 +0200

    Update cache version from v11 to v12

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'brw-v11';
+const CACHE = 'brw-v12';
 const ASSETS = ['/BRW-Garage/', '/BRW-Garage/index.html', '/BRW-Garage/manifest.json', '/BRW-Garage/icon-192.png', '/BRW-Garage/icon-512.png'];
 self.addEventListener('install', e => {
   e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS).catch(() => {})));
